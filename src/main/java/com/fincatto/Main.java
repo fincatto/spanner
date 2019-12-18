4dcf1de737dfea2f39b474e34a0b737609e65d1a
Diego Fincatto <diego@fincatto.com>
2019-12-18 11:00:56 -0300

    Adicionando primeiros teste de unidade.

---
+++ src/main/java/com/fincatto/Main.java
@@ -0,0 +1,6 @@
+package com.fincatto;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Projeto de teste do Cloud Spanner!");
+    }
+}
---
+++ src/test/java/com/fincatto/MainTest.java
@@ -0,0 +1,10 @@
+package com.fincatto;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void iniciaConexaoTest() {
+        System.out.println("Iniciando...");
+    }
+}
